181dc8402a4c9a34382da76104073a31463b775b
Jay Mistry <jaymistry258@gmail.com>
2023-06-27 11:52:28 +0000

    phoenix: Bringup Project-Xtended
    Signed-off-by: Jay Mistry <jaymistry258@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_phoenix.mk
+    $(LOCAL_DIR)/xtended_phoenix.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_phoenix-user \
-    evolution_phoenix-userdebug \
-    evolution_phoenix-eng
+    xtended_phoenix-user \
+    xtended_phoenix-userdebug \
+    xtended_phoenix-eng
--- xtended_phoenix.mk
+++ xtended_phoenix.mk
@@ -12,20 +12,25 @@ $(call inherit-product, device/xiaomi/phoenix/device.mk)
 # Camera
 $(call inherit-product-if-exists, device/xiaomi/phoenix-miuicamera/config.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common Project-Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
-# Evolution X Build Type & Maintainer stuff.
-EVO_BUILD_TYPE := Unofficial
+# Project-Xtended Build Type & Maintainer stuff.
+XTENDED_BUILD_TYPE := OFFICIAL
+XTENDED_BUILD_MAINTAINER := JAYMISTRY258
+
+# SOC
+PROCESSOR_MODEL := SM6150
 
 # Gapps Build Details.
-TARGET_USES_MINNI_GAPPS := true
+WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_NGA := true
 
 # Custom stuff.
 TARGET_BUILD_APERTURE_CAMERA := false
-TARGET_USE_PIXEL_FINGERPRINT := true
-TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_USE_PIXEL_FINGERPRINT := false
+TARGET_INCLUDE_PIXEL_CHARGER := false
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_ENABLE_BLUR := true
 
@@ -38,13 +43,13 @@ TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Build Fingerprint.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 13 TP1A.221005.002/9012097 release-keys"
-
-BUILD_FINGERPRINT := google/raven/raven:13/TP1A.221005.002/9012097:user/release-keys
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+#    PRIVATE_BUILD_DESC="raven-user 13 TP1A.221005.002/9012097 release-keys"
+#
+#BUILD_FINGERPRINT := google/raven/raven:13/TP1A.221005.002/9012097:user/release-keys
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_phoenix
+PRODUCT_NAME := xtended_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
